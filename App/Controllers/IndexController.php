50389c382962e6672fb3b96122f05876b276c609
Onesimo Batista <onesimobatista@gmail.com>
2016-09-25 14:54:07 -0300

    add view

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -3,13 +3,32 @@ namespace App\Controllers;
 
 class IndexController 
 {
+	private $views;
+
+	public function __construct()
+	{
+		   $this->views = new \stdClass;
+	}
+
 	public function index()
 	{
-		echo "Route: / controller index ";
+		$this->views->cars = array("Mustang", "Ferrari", "Lamborghini");
+		$this->render("index");
 	}
 
 	public function contact()
+	{	
+		$this->views->cars = array("Mustang", "Ferrari","Lamborghini");
+		$this->render("contact");
+	}
+
+	public function render($action)
 	{
-		echo "Route: / controller contact ";
+		$current = get_class($this);
+		$singleClassName = strtolower(str_replace("Controller","",
+			 str_replace("App\\Controllers\\","",$current)));
+
+		include_once "../App/Views/".$singleClassName."/".$action.".phtml";
+
 	}
 }
